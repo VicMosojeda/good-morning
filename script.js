6c22e8ec5f2d12b96d5e8c49434bab3153d1a313
metadeth <metadeth@Metadeth.metadeth>
2024-11-14 09:32:50 -0600

    good morning

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn_here = document.querySelector('.btn_here');
+const msg = document.getElementById('msg');
+
+const messages = [
+  "No olvides comer bien",
+  "Mantente bien hidratada",
+  "Trata de tomarte un descanso",
+  "Recuerda sonreír, !Es Gratis¡",
+  "Haz algo que te haga feliz hoy"
+];
+
+function getRandomMessage() {
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  return messages[randomIndex];
+}
+
+btn_here.addEventListener('click', () => {
+  const message = getRandomMessage();
+  msg.textContent = message;
+});
